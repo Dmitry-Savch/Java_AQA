50e91dc97ab50c4e95ce2a15bff39311701c659c
Dmitry-Savch <savchdimon@gmail.com>
2024-12-14 13:55:17 +0200

    finished task 20, task 9

--- Java_AQA3/src/main/java/task_6/Data.java
+++ Java_AQA3/src/main/java/task_6/Data.java
@@ -1,32 +1,32 @@
-package task_6;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table
-
-public class Data {
-//    <data>
-//  <name>Katie</name>
-//  <birthdate>1990-05-12</birthdate>
-//    <address>
-//    <street>123 Main St</street>
-//    <city>Denver</city>
-//    <state>CO</state>
-//    </address>
-//    </data>
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name = "id")
-private Long id;
-
-
-private String name;
-private String birthdate;
-private String address;
-private String street;
-private String city;
-private String state;
-
-}
+//package task_6;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table
+//
+//public class Data {
+////    <data>
+////  <name>Katie</name>
+////  <birthdate>1990-05-12</birthdate>
+////    <address>
+////    <street>123 Main St</street>
+////    <city>Denver</city>
+////    <state>CO</state>
+////    </address>
+////    </data>
+//@Id
+//@GeneratedValue(strategy = GenerationType.IDENTITY)
+//@Column(name = "id")
+//private Long id;
+//
+//
+//private String name;
+//private String birthdate;
+//private String address;
+//private String street;
+//private String city;
+//private String state;
+//
+//}
